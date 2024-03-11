a76fbefcb959605f3dc47fd0177f4b616e3c1d2f
Benjamin Fisico <BenjaminFisico@gmail.com>
2024-03-11 15:25:55 -0300

    git hub pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://benjaminFisico/my-portfolio.github.io',
+    base: '/my-portfolio',
+});
