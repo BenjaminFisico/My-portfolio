7634e45ff58ad21e79acb3768b159b2ac48ff22b
Benjamin Fisico <BenjaminFisico@gmail.com>
2024-03-11 15:43:10 -0300

    añadiendo page actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://benjaminFisico/my-portfolio.github.io',
-    base: '/my-portfolio',
+    site: 'https://benjaminFisico/my-portfolio.github.io'
 });
